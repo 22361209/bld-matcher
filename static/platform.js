85fe7bef9601bee590990ba2b3d88eb2b9bbe1a1
林振岳 <linzhenyue@Mac-mini.local>
2026-07-21 02:26:43 +0800

    Revert "fix: enlarge Windows list typography"
    This reverts commit dc3b19bdf64150b41b7020a9e65bd775f75ee1b3.

--- static/platform.js
+++
@@ -1,5 +0,0 @@
-const platform = navigator.userAgentData?.platform || navigator.platform || navigator.userAgent;
-
-if (/win/i.test(platform) || /Windows NT/i.test(navigator.userAgent)) {
-  document.documentElement.dataset.platform = "windows";
-}
